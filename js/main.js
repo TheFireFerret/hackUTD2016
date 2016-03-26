cdfb6debe60519ee284d5b6a4341400e5bcda1d9
Sami Glasco <samiglasco@gmail.com>
2016-03-26 16:56:51 -0500

    iteminfo file for item testing for now ??? internet pls

--- js/main.js
+++ js/main.js
@@ -7,6 +7,13 @@ function fblogin() {
 
         } else {
             console.log("Authenticated successfully with payload:", authData);
+            var npath = "profiles/";
+            npath += authData.facebook.id;
+            var pref = ref.child(npath);
+            var m = []
+            pref.set({
+                "name":authData.facebook.displayName
+            })
             window.location.href = 'owner.html'
         }
     });
@@ -16,3 +23,19 @@ function fblogout() {
     ref.unauth();
     window.location.href = 'index.html'
 }
+
+$(function () {
+    $('a[href*=#]:not([href=#])').click(function () {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') || location.hostname == this.hostname) {
+
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
+        }
+    });
+});
