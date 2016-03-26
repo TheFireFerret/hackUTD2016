95b05f09a1ba3edad196ad074dc389a34941bac9
Sami Glasco <samiglasco@gmail.com>
2016-03-26 17:58:28 -0500

    I DONT KNOW

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 var ref = new Firebase("https://keypin.firebaseio.com");
+var data;
 
 function fblogin() {
     ref.authWithOAuthPopup("facebook", function (error, authData) {
@@ -7,6 +8,7 @@ function fblogin() {
 
         } else {
             console.log("Authenticated successfully with payload:", authData);
+            data = authData.facebook.id;
             var npath = "profiles/";
             npath += authData.facebook.id;
             var pref = ref.child(npath);
@@ -19,11 +21,52 @@ function fblogin() {
     });
 }
 
+function fbloginNoRedirect() {
+    ref.authWithOAuthPopup("facebook", function (error, authData) {
+        if (error) {
+            console.log("Login Failed!", error);
+
+        } else {
+            console.log("Authenticated successfully with payload:", authData);
+            data = authData.facebook.id;
+            var npath = "profiles/";
+            npath += authData.facebook.id;
+            var pref = ref.child(npath);
+            var m = []
+            pref.set({
+                "name":authData.facebook.displayName
+            })
+        }
+    });
+}
+
 function fblogout() {
     ref.unauth();
     window.location.href = 'index.html'
 }
 
+function additem() {
+    ref.authWithOAuthPopup("facebook", function (error, authData) {
+        if (error) {
+            console.log("Login Failed!", error);
+
+        } else {
+            var npath = "items/";
+            var item_id = document.getElementById('item_id_form').value;
+            //console.log("ID: ", item_id);
+            //console.log("data: ", data);
+
+            var pref = ref.child(npath+item_id);
+            pref.set({
+                "name": document.getElementById('item_name_form').value,
+                "owner_id": authData.facebook.id
+            });
+        }
+    });
+}
+
+
+//slow scroll
 $(function () {
     $('a[href*=#]:not([href=#])').click(function () {
         if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') || location.hostname == this.hostname) {
