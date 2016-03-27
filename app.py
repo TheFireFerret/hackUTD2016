f7f605d2fa98bb181adabed7f2c2a289efc9d937
Sami Glasco <samiglasco@gmail.com>
2016-03-26 22:25:54 -0500

    heroku???

--- app.py
+++ app.py
@@ -35,5 +35,5 @@ def objects():
 
 if __name__ == '__main__':
     #manager.run()
-    app.run()
-    #(host='0.0.0.0', port=8080)
+    port= int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
